1235776bfb2ae5a169ef6b446f3e3db5ae58e7c3
Vac1911 <vac1911@outlook.com>
2021-08-30 17:05:00 -0400

    init

---
+++ app/app-server.d.ts
@@ -0,0 +1,9 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+import { initialData } from './module.js';
+export declare function renderAppWithInitialData(): Generator<string, void, undefined>;
+export declare function renderApp(data: typeof initialData): Generator<string, void, undefined>;
+//# sourceMappingURL=app-server.d.ts.map
